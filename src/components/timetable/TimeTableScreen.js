c95df2b6f334ac80899d691bf4c05ffba93517e5
Wilfredo Chacon <wilfredo.chacon@mycontolgps.com>
2022-06-30 10:35:12 -0600

    Actualizando

--- src/components/timetable/TimeTableScreen.js
+++ src/components/timetable/TimeTableScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, Image, TextInput, TouchableHighlight, FlatList, ActivityIndicator} from 'react-native';
+import { Text, View, StyleSheet, Image, TextInput, TouchableHighlight, FlatList, ActivityIndicator, Modal} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Http from 'smartstudent/src/libs/http';
@@ -15,6 +15,9 @@ class TimeTableScreen extends Component {
         sid:"",
         selectedValue:"Day",
         data:[],
+        isPlaformIOS:(Platform.OS==="ios"),
+        plaformIOS:Platform.OS,
+        isOpenDay:false,
         dataDay:[
             {
                 value:"Day",
@@ -84,6 +87,10 @@ class TimeTableScreen extends Component {
         return itemPicker;
     }
 
+    changeStatePickerDay(isOpenDay) {
+        this.setState({isOpenDay})
+    }
+
     render() {
         return (
             <View style={style.container}>
@@ -92,16 +99,25 @@ class TimeTableScreen extends Component {
                  top:200
                 }}/>
                 <View style={style.filter}>
-                    <Picker
-                        selectedValue={this.state.selectedValue}
-                        style={{ height: 50, flex:1, color:"#000"}}
-                        dropdownIconColor="#000"
-                        onValueChange={(itemValue, itemIndex) => {
-                            this.setState({selectedValue:itemValue});
-                        }}
-                    >
-                        {this.renderDay()}
-                    </Picker>
+                { (!this.state.isPlaformIOS ?  (
+                        <Picker
+                            selectedValue={this.state.selectedValue}
+                            style={{ height: 50, flex:1, color:"#000"}}
+                            dropdownIconColor="#000"
+                            onValueChange={(itemValue, itemIndex) => {
+                                this.setState({selectedValue:itemValue});
+                            }}
+                        >
+                            {this.renderDay()}
+                        </Picker>
+                    ) : <View style={style.btnPicker}>
+                            <TouchableHighlight onPress={()=>{
+                                    this.changeStatePickerDay(true);;
+                            }}>
+                                <Text style={style.textPicker}>{this.state.selectedValue}</Text>
+                            </TouchableHighlight>
+                        </View>
+                    )}
                     <TouchableHighlight onPress={()=>{
                             this.getData();
                     }}>
@@ -126,6 +142,41 @@ class TimeTableScreen extends Component {
                         }
                     />
                 </View>
+                <Modal
+                    transparent
+                    animationType="fade"
+                    visible={this.state.isOpenDay}
+                    onRequestClose={() => {
+                        Alert.alert('Modal has been closed.');
+                    }}>
+                    <View style={style.contentContainer}>
+                        <View style={style.modalView}>
+                            <Text style={{marginTop:10}}>Select Day</Text>
+                            <Picker
+                                selectedValue={this.state.selectedValue}
+                                style={{ height: 50, width:300, color:"#00f"}}
+                                dropdownIconColor="#000"
+                                onValueChange={(itemValue, itemIndex) => {
+                                    this.setState({selectedValue:itemValue});
+                                }}
+                            >
+                                {this.renderDay()}
+                            </Picker>
+                            <View style={{position:"relative",bottom:-150}}>
+                                <TouchableHighlight
+                                    style={style.confirmButton}
+                                    onPress={() => {
+                                        this.setState({isOpenDay:false});
+                                        }
+                                    }>
+                                    <View style={[style.btnSesion,{flex:0}]}>
+                                        <Text style={style.txtSession}>CONFIRM</Text>
+                                    </View>
+                                </TouchableHighlight>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
             </View>
         )
     }
@@ -140,6 +191,55 @@ const style=StyleSheet.create({
     filter: {
         //flex:1,
         flexDirection:"row"
+    },
+    btnPicker:{
+        backgroundColor:"#ccc",
+        height:40,
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        marginTop:10,
+        marginLeft:10        
+    },
+    textPicker: {
+        fontSize:14,
+        fontWeight:"bold"
+    },
+    contentContainer: {
+        //height:200,
+        justifyContent:"center",
+        alignItems:"center",
+        marginTop:22
+    },
+    modalView: {
+        margin:20,
+        width:300,
+        height:290,
+        backgroundColor:"#fff",
+        borderRadius:20,
+        //padding:20,
+        alignItems:"center",
+        shadowColor:"#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    btnSesion: {
+        marginTop:12,
+        marginLeft:"auto",
+        marginRight:5,
+        marginBottom:12,
+        padding:5,
+        backgroundColor:"#01B9F5",
+        height:30,
+        flex:1,
+        alignItems:"center",
+        justifyContent:"center",
+        alignSelf:"flex-start"
     },
     calendar: {
         width:30,
